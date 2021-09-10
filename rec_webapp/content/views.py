d601417b759b5478863528f6905b9448228794c1
Aditya Agarwal <aditya.ag1234@gmail.com>
2021-09-10 18:41:11 +0530

    API

--- rec_webapp/content/views.py
+++ rec_webapp/content/views.py
@@ -3,22 +3,27 @@ from django.http import HttpResponse
 
 import os
 import requests
-print('key-->',os.getenv('x_rapidapi_key'))
+
+
+base_url = "https://jikan1.p.rapidapi.com/"
+
+headers = {
+    'x-rapidapi-host': "jikan1.p.rapidapi.com",
+    'x-rapidapi-key': os.getenv('x_rapidapi_key')
+}
+
+
+def search_anime(query: str):
+    url = base_url + "/search/anime"
+    querystring = {"q": query}
+    return requests.request("GET", url, headers=headers, params=querystring)
+
+
+def top_anime(page: int):
+    url = base_url + "/top/anime/" + str(page) + "/airing"
+    return requests.request("GET", url, headers=headers)
+
+
 def home_page(request):
-
-    print("REQUEST: ", request)
-
-    url = "https://jikan1.p.rapidapi.com/search/anime"
-
-    querystring = {"q": "sword"}
-
-    headers = {
-        'x-rapidapi-host': "jikan1.p.rapidapi.com",
-        'x-rapidapi-key': os.getenv('x_rapidapi_key')
-    }
-    
-    
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring)
-    return HttpResponse(response.text)
-    # return 0
+    response = top_anime(1)
+    return render(request, 'index.html', {'content': response.text})
