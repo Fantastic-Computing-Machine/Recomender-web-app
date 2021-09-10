3a8409bf14cb84568f53a653c97289a71522c754
Nilesh KM <42093170+ZEROCONNECT@users.noreply.github.com>
2021-09-10 18:28:01 +0530

    Update views.py

--- rec_webapp/content/views.py
+++ rec_webapp/content/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 import os
 import requests
-
+print('hello world')
 def home_page(request):
 
     print("REQUEST: ", request)
@@ -16,7 +16,8 @@ def home_page(request):
         'x-rapidapi-host': "jikan1.p.rapidapi.com",
         'x-rapidapi-key': os.getenv('x_rapidapi_key')
     }
-
+    
+    
     response = requests.request(
         "GET", url, headers=headers, params=querystring)
     return HttpResponse(response.text)
